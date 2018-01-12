db76b651bd82c600821841dc8cf27dc0d77df327
Abdul-Rahim <fabdul@st.vvu.edu.gh>
2018-01-12 15:10:52 +0000

    creating table for students list

--- src/pages/list/list.module.ts
+++ src/pages/list/list.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-//import { ListPage } from './list';
+import { ListPage } from './list';
 
 @NgModule({
   declarations: [
